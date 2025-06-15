99216f9fb2b00df493fa4f61b9ac78c6c53f1549
emerson000 <94085544+emerson000@users.noreply.github.com>
2025-06-14 20:10:17 -0400

    chore: update package names and identifiers across Android, iOS, Linux, and macOS platforms

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.taskwire"
+    namespace = "io.taskwire.android"
     compileSdk = flutter.compileSdkVersion
     // ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.taskwire"
+        applicationId = "io.taskwire.android"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
