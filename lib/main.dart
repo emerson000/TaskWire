0e7387eb2b0649bae753d03fc12cff444348ccfb
emerson000 <emerson@centercad.com>
2025-06-22 21:33:29 -0400

    ui: disable debug banner in main application

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TaskWire',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
