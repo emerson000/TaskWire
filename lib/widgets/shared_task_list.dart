bc5e57fa3b0b5294f8f478742811cd26b4778b1e
emerson000 <94085544+emerson000@users.noreply.github.com>
2025-07-04 09:53:56 -0400

    fix: add task tile spacing

--- lib/widgets/shared_task_list.dart
+++ lib/widgets/shared_task_list.dart
@@ -94,70 +94,69 @@ class _SharedTaskListState extends State<SharedTaskList> {
       );
     }
 
-    return Column(
-      children: [
-        Expanded(
-          child: ReorderableListView.builder(
-            buildDefaultDragHandles: false,
-            itemCount: _displayTasks.length,
-            onReorder: (oldIndex, newIndex) {
-              if (widget.onReorderTasks != null) {
-                if (newIndex > oldIndex) {
-                  newIndex -= 1;
-                }
-                
-                setState(() {
-                  final task = _displayTasks.removeAt(oldIndex);
-                  _displayTasks.insert(newIndex, task);
-                });
-                
-                if (widget.onOptimisticReorder != null) {
-                  widget.onOptimisticReorder!(_displayTasks);
-                }
-                
-                widget.onReorderTasks!(widget.parent?.id.toString(), oldIndex, newIndex);
-              }
-            },
-            itemBuilder: (context, index) {
-              final task = _displayTasks[index];
-              return DragTarget<Task>(
+    return SizedBox.expand(
+      child: ReorderableListView.builder(
+        buildDefaultDragHandles: false,
+        itemCount: _displayTasks.length + (shouldShowAddTask ? 1 : 0),
+        onReorder: (oldIndex, newIndex) {
+          if (widget.onReorderTasks != null) {
+            if (newIndex > oldIndex) {
+              newIndex -= 1;
+            }
+            
+            setState(() {
+              final task = _displayTasks.removeAt(oldIndex);
+              _displayTasks.insert(newIndex, task);
+            });
+            
+            if (widget.onOptimisticReorder != null) {
+              widget.onOptimisticReorder!(_displayTasks);
+            }
+            
+            widget.onReorderTasks!(widget.parent?.id.toString(), oldIndex, newIndex);
+          }
+        },
+        itemBuilder: (context, index) {
+          if (shouldShowAddTask && index == _displayTasks.length) {
+            return AddTaskTile(
+              key: const ValueKey('add-task-tile'),
+              parentId: widget.parent?.id.toString(),
+              parentTitle: widget.parent?.title,
+              onAddTask: widget.onCreateTask,
+              onAddMultipleTasks: widget.onAddMultipleTasks,
+              onCancel: widget.onHideAddTask,
+            );
+          }
+          
+          final task = _displayTasks[index];
+          return DragTarget<Task>(
+            key: ValueKey(task.id),
+            onWillAcceptWithDetails: (details) => details.data.id != task.id,
+            onAcceptWithDetails: (details) => widget.onTaskDrop(details.data, task),
+            builder: (context, candidateData, rejectedData) {
+              return TaskTile(
                 key: ValueKey(task.id),
-                onWillAcceptWithDetails: (details) => details.data.id != task.id,
-                onAcceptWithDetails: (details) => widget.onTaskDrop(details.data, task),
-                builder: (context, candidateData, rejectedData) {
-                  return TaskTile(
-                    key: ValueKey(task.id),
-                    task: task,
-                    isEditing: widget.editingTaskId == task.id,
-                    isSelected: widget.isSelected?.call(task) ?? false,
-                    editController: widget.editController,
-                    onTap: () => widget.onTaskTap(task),
-                    onEdit: () => widget.onStartEditing(task),
-                    onDelete: () => widget.onDeleteTask(task.id),
-                    onCheckboxChanged: (_) => widget.onUpdateTask(
-                      task.id,
-                      isCompleted: !task.isCompleted,
-                    ),
-                    onEditComplete: widget.onFinishEditing,
-                    onEditCancel: widget.onEditCancel,
-                    isDragTarget: candidateData.isNotEmpty,
-                    onDragAccept: (draggedTask) => widget.onTaskDrop(draggedTask, task),
-                    reorderIndex: index,
-                  );
-                },
+                task: task,
+                isEditing: widget.editingTaskId == task.id,
+                isSelected: widget.isSelected?.call(task) ?? false,
+                editController: widget.editController,
+                onTap: () => widget.onTaskTap(task),
+                onEdit: () => widget.onStartEditing(task),
+                onDelete: () => widget.onDeleteTask(task.id),
+                onCheckboxChanged: (_) => widget.onUpdateTask(
+                  task.id,
+                  isCompleted: !task.isCompleted,
+                ),
+                onEditComplete: widget.onFinishEditing,
+                onEditCancel: widget.onEditCancel,
+                isDragTarget: candidateData.isNotEmpty,
+                onDragAccept: (draggedTask) => widget.onTaskDrop(draggedTask, task),
+                reorderIndex: index,
               );
             },
-          ),
-        ),
-        if (shouldShowAddTask)
-          AddTaskTile(
-            parentId: widget.parent?.id.toString(),
-            parentTitle: widget.parent?.title,
-            onAddTask: widget.onCreateTask,
-            onAddMultipleTasks: widget.onAddMultipleTasks,
-            onCancel: widget.onHideAddTask,
-          ),
-      ],
+          );
+        },
+      ),
     );
   }
 } 
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import '../models/task.dart';
 import '../utils/platform_utils.dart';
